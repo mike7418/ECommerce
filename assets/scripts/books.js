ddee4e90b952f32f6b43e170ede1481e004d2531
mike7418 <parksmike@dwx.com>
2026-03-21 12:03:11 -0500

    Adding books page content

---
+++ assets/scripts/books.js
@@ -0,0 +1,60 @@
+// JavaScript source code
+
+let books;
+
+async function renderBooks(filter) {
+    const booksWrapper = document.querySelector(".books");
+
+    booksWrapper.classList += ' books__loading'
+
+    if (!books) {
+        books = await getBooks();
+    }
+
+    booksWrapper.classList.remove('books__loading')
+
+    if (filter === "LOW_TO_HIGH") {
+        books.sort(
+            (a, b) =>
+                (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)
+        );
+    } else if (filter === "HIGH_TO_LOW") {
+        books.sort(
+            (a, b) =>
+                (b.salePrice || b.originalPrice) - (a.salePrice || a originalPrice)
+        );
+    } else if (filter === "RATING") {
+        books.sort((a, b) => b.rating - a.rating);
+    }
+
+    const booksHtml = books
+        .map((book) => {
+            return `<div class="book">
+    <figure class="book__img--wrapper">
+      <img class="book__img" src="${book.url}" alt="">
+    </figure>
+    <div class="book__title">
+      ${book.title}
+    </div>
+    <div class="book__ratings">
+      ${ratingsHTML(book.rating)}
+    </div>
+    <div class="book__price">
+      ${priceHTML(book.originalPrice, book.salePrice)}
+    </div>
+  </div>`;
+        })
+        .join("");
+
+    booksWrapper.innerHTML = booksHtml;
+}
+
+function priceHTML(originalPrice, salePrice) {
+    if (!salePrice) {
+        return '$${originalPrice.toFixed(2)}`;
+    }
+    return `<span class="book__price--normal">$${originalPrice.toFixed(
+        2
+    )}</span>$${salePrice.toFixed(2)}`;
+}
+
